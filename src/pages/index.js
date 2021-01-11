21b6073cb8b7a75914dc16b302f248aa9652b216
jvitorof <j.vitoroflores@gmail.com>
2021-01-11 20:26:44 -0300

    :metal: Adicionando estrutura ao projeto :rocket:

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Auth from './Auth';
+import Config from './Config';
+import Feedback from './Game';
+import Game from './Game';
+import Login from './Login';
+import Ranking from './Ranking';
+
+export { Auth, Config, Feedback, Game, Login, Ranking };
