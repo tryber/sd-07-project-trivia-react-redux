03b2c99018507982dcf2451b2c61e83e4258cdf8
Flavio Oliveira <flavsoliver@gmail.com>
2021-01-11 19:37:45 -0300

    configuração inicial e requisito 01

---
+++ src/pages/login/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.validateEmail = this.validateEmail.bind(this);
+
+    this.state = {
+      name: '',
+      gravatarEmail: '',
+      isButtonAble: false,
+    };
+  }
+
+  validateEmail(email) {
+    let isButtonAble = false;
+    const re = /[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i;
+    if (re.test(email.toLowerCase())) {
+      isButtonAble = true;
+    }
+    this.setState({
+      email,
+      isButtonAble,
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          data-testid="input-player-name"
+          name="name"
+          placeholder="Enter your name"
+          value={ name }
+        />
+        <input
+          type="text"
+          data-testid="input-gravatar-email"
+          name="gravatarEmail"
+          value={ gravatarEmail }
+        />
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ isButtonAble }
+        >
+          Jogar
+        </button>
+      </form>
+    );
+  }
+}
+
+export default login;
