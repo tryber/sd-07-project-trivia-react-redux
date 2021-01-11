d47a3f58295424b632377f368bb47a9c56b65ed8
Thays Costa <thayscosta3@gmail.com>
2021-01-11 19:02:42 -0300

    Requirement nº 3 - all together

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import Settings from './Pages/Settings';
 
 export default function App() {
   return (
-    <div className="App">
-      <Login />
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/settings" component={ Settings } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
