f7dc0459c6db880c08a2b7b66042713307199fe5
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-01-11 19:13:24 -0300

    Modificado store

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,6 @@ const composed = compose(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-const store = createStore(reducer, composed);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
