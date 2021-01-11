dd8fc6f918b2a1b5b1a587f656e12f56e80f7dc1
Márcio Barroso <barrrosomarcio@gmail.com>
2021-01-11 18:19:23 -0300

    completion req 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './trivia.png';
 import './App.css';
-import Login from './pages/Login';
 import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
-        <Route to='/' component={Login}/>
+        <Route to="/" component={ Login } />
       </Switch>
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,64 +2,67 @@ import React, { Component } from 'react';
 import './login.css';
 
 class Login extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
     this.valida = this.valida.bind(this);
 
     this.state = {
-      name:'',
-      email:'',
+      name: '',
+      email: '',
     };
   }
-  handleChange(e){
+
+  handleChange(e) {
     const { value, name } = e.target;
-    this.setState({ [name]:value });
-  };
-  valida(){
+    this.setState({ [name]: value });
+  }
+
+  valida() {
     // eslint-disable-next-line
     const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
     const { email, name } = this.state;
-  
-    if(pattern.test(email) && name.length > 0) {
+    if (pattern.test(email) && name.length > 0) {
       return false;
     }
     return true;
   }
-  render(){
-    return(
+
+  render() {
+    return (
       <form>
         <label
-          htmlFor='name'
+          htmlFor="name"
         >
           Name:
           <input
-            name='name'
-            type='name'
-            data-testid='input-player-name'
-            onChange={this.handleChange}
+            name="name"
+            type="text"
+            data-testid="input-player-name"
+            onChange={ this.handleChange }
           />
         </label>
         <label
-          htmlFor='email'
+          htmlFor="email"
         >
           Email:
           <input
-            name='email'
-            type='email'
-            data-testid='input-gravatar-email'
-            onChange={this.handleChange}
+            name="email"
+            type="email"
+            data-testid="input-gravatar-email"
+            onChange={ this.handleChange }
           />
         </label>
         <button
-          data-testid='btn-play'
-          disabled={this.valida()}
+          type="button"
+          data-testid="btn-play"
+          disabled={ this.valida() }
         >
           Jogar
         </button>
       </form>
-    )
+    );
   }
 }
 
