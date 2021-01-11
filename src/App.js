177730110ea471fbe99fea2e1026386e8bbc65bc
alvaro <alvarovascon2@gmail.com>
2021-01-11 16:43:57 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
       </header>
     </div>
   );
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  isFetching: false,
+  token: '',
+  questions: [],
+};
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default reducer;
