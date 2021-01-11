c66c5b3fb3c201165fbcd87f94421fd97f1f80df
Bruno <bruno_grb@hotmail.com>
2021-01-11 19:10:46 -0300

    corrigindo eslint e adcionando token no localStorage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
-import Login from './Pages/Login'
+import Login from './Pages/Login';
 
 export default function App() {
   return (
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import fetchPlayeToken from '../helpers';
 
 class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       name: '',
       email: '',
       buttonDisable: true,
-    }
+    };
     this.enableButton = this.enableButton.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.verificationEmail = this.verificationEmail(this);
+    this.addToken = this.addToken.bind(this);
   }
 
   verificationEmail() {
@@ -19,17 +21,27 @@ class Login extends React.Component {
     return regex.test(email);
   }
 
-  handleChange({ target }){
+  handleChange({ target }) {
     const { name, value } = target;
     this.setState({ [name]: value }, () => {
       this.enableButton();
     });
   }
 
+  addToken() {
+    const fetchToken = fetchPlayeToken();
+    const { token } = fetchToken;
+    if (Storage) {
+      const tokens = JSON.parse(localStorage.getItem('token'));
+      const values = tokens === null ? [] : tokens;
+      values.push(token);
+      localStorage.setItem('token', JSON.stringify(values));
+    }
+  }
+
   enableButton() {
     const { name } = this.state;
-    
-    if(name.length !== 0 && this.verificationEmail()){
+    if (name.length !== 0 && this.verificationEmail()) {
       return this.setState({ buttonDisable: false });
     }
     this.setState({ buttonDisable: true });
@@ -37,32 +49,34 @@ class Login extends React.Component {
 
   render() {
     const { buttonDisable, name, email } = this.state;
-    return(
+    return (
       <div>
         <input
-        type="text"
-        name="name"
-        value={ name }
-        placeholder="insira seu nome"
-        data-testid="input-player-name"
-        onChange={ this.enableButton }
+          type="text"
+          name="name"
+          value={ name }
+          placeholder="insira seu nome"
+          data-testid="input-player-name"
+          onChange={ this.enableButton }
         />
         <input
-        type="text"
-        name="email"
-        value={ email }
-        placeholder="insira seu email"
-        data-testid="input-gravatar-email"
-        onChange={ this.enableButton }
+          type="text"
+          name="email"
+          value={ email }
+          placeholder="insira seu email"
+          data-testid="input-gravatar-email"
+          onChange={ this.enableButton }
         />
         <button
-        type="button"
-        data-testid="btn-play"
-        name="buttonDisable"
-        disabled={ buttonDisable }
-        >Jogar</button>
+          type="button"
+          data-testid="btn-play"
+          name="buttonDisable"
+          disabled={ buttonDisable }
+        >
+          Jogar
+        </button>
       </div>
-    )
+    );
   }
 }
 
