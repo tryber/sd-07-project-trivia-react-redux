e498201943f5207680989c25a6a06279f739c06b
Jr3564 <rodolfojr94@gmail.com>
2021-01-11 17:29:37 -0300

    INITIAL_STATE

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
-          31
+          ASDFJKASDF
         </p>
       </header>
     </div>
