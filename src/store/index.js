684090f2e0622dd05ff2705a223dd33b7aa78ad1
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-01-11 19:16:03 -0300

    Adicionando a maracutaia

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers';
 
-const composed = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-const store = createStore(reducer, applyMiddleware(thunk));
+const maracutaia = (desenvolvendo = true) => {
+  if (desenvolvendo === true) {
+    const composed = compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    );
+    return composed;
+  }
+  return applyMiddleware(thunk);
+};
+
+const store = createStore(reducer, maracutaia(false));
 
 export default store;
