e8a7c646c475c3451a0599a210d58b30ea9141b4
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-01-11 18:24:56 -0300

    email validation

--- src/pages/login.js
+++ src/pages/login.js
@@ -9,6 +9,7 @@ class Login extends Component {
     this.state = {
       name: '',
       email: '',
+      auth: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.click = this.click.bind(this);
@@ -16,6 +17,15 @@ class Login extends Component {
 
   handleChange({ target: { name, value } }) {
     this.setState({ [name]: value });
+    this.emailValidation();
+  }
+
+  emailValidation() {
+    const { email, name, auth } = this.state;
+    const magicNumber = 0;
+    if (email.match(/\S+@\S+\.\S+/) && name.length > magicNumber) {
+      return this.setState({ [auth]: true });
+    }
   }
 
   click() {
@@ -26,7 +36,7 @@ class Login extends Component {
   }
 
   render() {
-    const { name, email } = this.state;
+    const { name, email, auth } = this.state;
     return (
       <div>
         <input
@@ -41,7 +51,14 @@ class Login extends Component {
           data-testid="input-gravatar-email"
           onChange={ (event) => this.handleChange(event) }
         />
-        <button data-testid="btn-play" type="button" onClick={ this.click }>Jogar</button>
+        <button
+          data-testid="btn-play"
+          type="button"
+          onClick={ this.click }
+          disabled={ !auth }
+        >
+          Jogar
+        </button>
       </div>
     );
   }
