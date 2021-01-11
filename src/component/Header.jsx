3fbbac33b5d11ad39db4fe43106b6707c847a5b7
luc-zago <luc.zago@hotmail.com>
2021-01-11 17:53:56 -0300

    Criadas as bases para os requisitos 4, 12, 13 e 14

---
+++ src/component/Header.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { imgSource, playerName, playerScore } = this.props;
+    return (
+      <header>
+        <div className="avatar-container">
+          <img
+            src={ imgSource }
+            data-testid="header-profile-picture"
+          />
+        </div>
+        <div className="name-container">
+          <p data-testid="header-player-name">
+            { playerName }
+          </p>
+        </div>
+        <div className="score-container">
+          <p data-testid="header-score">
+            { playerScore }
+          </p>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
+
+CurrencyOption.propTypes = {
+  src: propTypes.string,
+  name: propTypes.string,
+  score: propTypes.number,
+}.isRequired;
---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Header from '../component/Header';
+import propTypes from 'prop-types';
+
+class Feedback extends Component {
+  constructor(props) {
+    super(props);
+    this.renderMessage = this.renderMessage.bind(this);
+  }
+
+  renderMessage(correctQuestions) {
+    if (correctQuestions < 3) {
+      return (<h1 data-testid="feedback-text">Podia ser melhor...</h1>)
+    }
+    return (<h1 data-testid="feedback-text">Mandou bem!</h1>)
+  }
+
+  render() {
+    const { src, name, score, correctQuestions } = this.props;
+    return (
+      <div>
+        <Header
+          imgSource={ src }
+          playerName={ name }
+          playerScore={ score }
+        />
+        { this.renderMessage(correctQuestions) }
+        <div className="score-questions-container">
+          <h3
+            data-testid="feedbakc-total-quesiton"
+          >
+            Você acertou { correctQuestions } questões!
+          </h3>
+          <h3
+            data-testid="feedback-total-score"
+          >
+            Um total de { score } pontos!
+          </h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
+
+Feedback.propTypes = {
+  src: propTypes.string,
+  name: propTypes.string,
+  score: propTypes.number,
+  correctQuestions: propTypes.number,
+}.isRequired;
