ccb3043447e4181657b2c3be01a36995163c9c7e
PedroMarqDev <PedroMarqDev@gmail.com>
2021-01-11 19:05:40 -0300

    BLA  BLA

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
+import api from './Api';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const { returnToken, returnAsks } = api;
+    const result = await returnToken();
+    const ask = await returnAsks(result);
+    console.log(ask)
+    console.log(result);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            SUA VEZ
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
+
+export default App;
