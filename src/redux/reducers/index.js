086881d2117ebf7fd96fdc6b8dd09c8ed7192b6d
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-01-12 19:39:10 -0300

    commitao

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import playerReducer from './playerReducer';
 import questionReducer from './questionReducer';
 import tokenReducer from './tokenReducer';
+import timerReducer from './timerReducer';
 
 export default combineReducers({
   playerReducer,
   questionReducer,
   tokenReducer,
+  timerReducer,
 });
