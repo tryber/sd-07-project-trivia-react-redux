9e27eadbe52df18510ebb2721ffb1073968bbadd
vanderson-henrique <vhbenedito@gmail.com>
2021-01-12 10:15:35 -0300

    Requisito 4 - Header com informações do Jogador

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../Components/Header';
 
 export default class Game extends Component {
   render() {
     return (
       <div>
         <h1>Tela de Jogo</h1>
+        <Header />
       </div>
     );
   }
