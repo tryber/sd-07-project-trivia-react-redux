915e61c519e58d26252b209fa397ec4a5f80c72a
Felipe Farina <wfelipefarina@gmail.com>
2021-01-12 15:52:02 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <Switch>
+          <Route path="/settings" component={ Settings } />
+          <Route exact path="/" component={ Login } />
+        </Switch>
       </header>
     </div>
   );
