56a533e95b76c2ec7f084d33a1eff43459eb194f
Eduardo Pizorno <pizorno@gmail.com>
2021-01-12 19:18:07 -0300

    Requisito 05 concluido - cypress ok

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,46 +1,75 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button } from 'semantic-ui-react';
 import { fetchQuestions } from '../actions';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.renderAllDataQuestion = this.renderAllDataQuestion.bind(this);
+    this.state = {
+      questionIndex: 0,
+    };
+  }
+
   componentDidMount() {
     const { getQuestions } = this.props;
     getQuestions();
   }
 
   renderAllDataQuestion() {
+    const { questionIndex } = this.state;
     const { questions } = this.props;
-    const { incorrect_answers, correct_answer, category, question } = this.question.results[0];
-    const arrayAnswers = [...incorrect_answers, correct_answer];
-  
+    if (questions.results) {
+      const correctAnswer = (
+        <button type="button" data-testid="correct-answer">
+          { questions.results[questionIndex].correct_answer }
+        </button>
+      );
+      const wrongAnswer = questions.results[
+        questionIndex
+      ].incorrect_answers.map((answer, index) => (
+        <button type="button" key={ answer } data-testid={ `wrong-answer-${index}` }>
+          {answer}
+        </button>
+      ));
+      return [correctAnswer, ...wrongAnswer];
+    }
   }
 
   render() {
-    return (questions.results)
-      ? (
-        <div>
-          <h1>TELA DE JOGO</h1>
-          <h3 data-testid="question-category">{category}</h3>
-          <h2 data-testid="question-text">{question}</h2>
-          <div>
-            {/* {
-              arrayAnswers.map((answer) => <button type="button" key={ answer }>{ answer }</button>)
-            } */}
-          </div>
-        </div>
-      ) : <p>loading</p>;
+    const { questionIndex } = this.state;
+    const { questions } = this.props;
+    return questions.results ? (
+      <div>
+        <h1>TELA DE JOGO</h1>
+        <h3 data-testid="question-category">
+          {questions.results[questionIndex].category}
+        </h3>
+        <h2 data-testid="question-text">
+          {questions.results[questionIndex].question}
+        </h2>
+        <div>{this.renderAllDataQuestion()}</div>
+      </div>
+    ) : (
+      <p>loading</p>
+    );
   }
 }
 const mapStateToProps = ({ gameReducer }) => ({
   questions: gameReducer.questions,
   isFetching: gameReducer.isFetching,
-
 });
 
 const mapDispatchToProps = (dispatch) => ({
   getQuestions: () => dispatch(fetchQuestions()),
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(Game);
 
-// descobrir pq o estado inicial de questions ta vindo undefined
+Game.propTypes = {
+  questions: PropTypes.shape({
+    results: PropTypes.arrayOf(Object).isRequired,
+  }).isRequired,
+  getQuestions: PropTypes.func.isRequired,
+};
