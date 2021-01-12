d644e933ce2c1c18b6a6531a9d3a4ff9e47aacc5
Francisco <franciscoliratorres@gmail.com>
2021-01-12 16:09:46 -0300

    Primeira tentativa - Requisito 3

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import GameScreen from '../pages/GameScreen';
+import Settings from '../pages/Settings';
 
 export default class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Routes extends Component {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/game" component={ GameScreen } />
+          <Route path="/settings" component={ Settings }/>
         </Switch>
       </BrowserRouter>
     );
