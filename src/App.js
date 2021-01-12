e423d4e5ffea60c783e2d32b99055778c55d49a9
Lucas <lafdesouza2002@outlook.com>
2021-01-12 00:50:33 -0300

    Adicionando botao settings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/settings" component={ Settings } />
         </Switch>
-      </BrowserRouter>
     </div>
   );
 }
