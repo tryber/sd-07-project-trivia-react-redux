1762141e16dc3058de43cc6b104e3787f1f36042
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-12 16:30:33 -0300

    Criando os componentes que sarao usados -> GridQuestion

---
+++ src/components/GridQuestions.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class GridQuestions extends Component {
+  render() {
+    return (
+      <div>
+        <Question />
+        <Answer />
+      </div>
+    );
+  }
+}
