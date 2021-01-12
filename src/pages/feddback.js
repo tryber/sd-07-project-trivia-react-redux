1fb3fc6f0395e965b760b134e3693b7446cb773f
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-01-12 18:59:44 -0300

    requisito 17 incompleto

---
+++ src/pages/feddback.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Header from './play';
+
+class Feedback extends Component {
+  constructor() {
+    super();
+    this.renderAssertionsText = this.renderAssertionsText.bind(this);
+  }
+
+  renderAssertionsText(assertions) {
+    return (
+      <p>
+        Você acertou
+        <span data-testid="feedback-total-question"> 
+          { assertions }
+        </span>
+        Perguntas
+      </p>
+    );
+  }
+
+  render() {
+    const { renderAssertionsText } = this;
+    const { assertions, score } = this.props;
+    const three = 3;
+    return (
+      <div>
+        <Header />
+        <section data-testid="feedback-text">
+          { renderAssertionsText(assertions) }
+          { assertions < three ? <h1>Podia ser melhor...</h1> : <h1>Mandou bem!</h1> }
+          <h1>
+            Você conseguiu
+            <span data-testid="feedback-total-score">
+              { score }
+            </span>
+            pontos!!!
+          </h1>
+        </section>
+        <Link to="/" data-testid="btn-play-again">
+          Jogar novamente
+        </Link>
+        <Link to="/ranking" data-testid="btn-ranking">
+          Ver Ranking
+        </Link>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  assertions: state.Play.assertions,
+  score: state.Play.score,
+};
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps) (Feedback);
