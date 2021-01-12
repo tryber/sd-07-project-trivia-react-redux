baaec2db8583b87cda583f7986051a40911f57de
paula-jfe <paula.jfe@gmail.com>
2021-01-12 18:33:26 -0300

    Requisito 3 ok

---
+++ src/pages/Settings.js
@@ -0,0 +1,25 @@
+import React from 'react';
+/* import { connect } from 'react-redux'; */
+
+class Settings extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+    /* const { token } = this.props; */
+    return (
+      <div>
+        <h1
+          data-testid="settings-title"
+        >
+          d
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Settings;
