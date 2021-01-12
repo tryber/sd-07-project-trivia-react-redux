da4e1802a74fc5f23c958fb5cbd60450c5f45852
Luiz Filipe <lfsmariz@gmail.com>
2021-01-12 09:53:16 -0300

    define correct Route Login Route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ LoginPage } />
     </Switch>
-  </BrowserRouter>;
+  </BrowserRouter>
 );
 
 export default Routes;
