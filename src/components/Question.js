52aead4cfb21f4aebd4857a75c611dcefa2e2627
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-12 16:34:58 -0300

    Criando os componentes que sarao usados -> Answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Question extends Component {
   render() {
     return (
       <div>
-        Question
+        Questions
       </div>
     );
   }
