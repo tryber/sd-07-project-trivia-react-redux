1ac2cdd65734fcf400ab0e2046ea1c857ed43300
Vitor Rodrigues <vitor_rc@outlook.com>
2021-01-12 15:32:16 -0300

    Inicio do requisito 12

---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div>
+        Tela de FeedBack
+      </div>
+    );
+  }
+}
+
+export default Feedback;
