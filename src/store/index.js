2958bb6094ff2a8e4c8b76561475fd7c2b8b036f
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-01-12 13:16:23 -0300

    Envio do requisito 2 com dependencia composeWithDevTools instalada

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
 const store = createStore(
   rootReducer,
-  compose(
+  composeWithDevTools(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
 
