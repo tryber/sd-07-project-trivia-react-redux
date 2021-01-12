0bfcc9ef6270edf02fb9e79fe96cbed6797b3e35
Flavio Oliveira <flavsoliver@gmail.com>
2021-01-12 18:26:11 -0300

    requisito 14 - exibição do score

--- src/pages/FeedBack/index.js
+++ src/pages/FeedBack/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../../components';
 
 class FeedBack extends Component {
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const threeAssertions = 3;
     return (
       <div>
@@ -14,6 +15,22 @@ class FeedBack extends Component {
             ? 'Mandou bem!'
             : 'Podia ser melhor...'}
         </p>
+        <p>Pontuação: </p>
+        <p data-testid="feedback-total-score">
+          {score}
+        </p>
+        <p> Acertos: </p>
+        <p data-testid="feedback-total-question">
+          {assertions}
+        </p>
+        <Link to="/">
+          <button
+            data-testid="btn-play-again"
+            type="button"
+          >
+            Jogar Novamente
+          </button>
+        </Link>
       </div>
     );
   }
@@ -21,6 +38,7 @@ class FeedBack extends Component {
 
 FeedBack.propTypes = {
   assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
 };
 
 export default FeedBack;
