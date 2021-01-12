8e88827481eed4af5c5a3ed4b9eddd691ee0ad3f
Henrique Coppola <hvenses@gmail.com>
2021-01-12 18:38:58 -0300

    Commitando requisito 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import Settings from './Pages/Settings';
+import User from './Components/User';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route exact path="/user" component={ User } />
           <Route path="/settings" component={ Settings } />
         </Switch>
       </BrowserRouter>
---
+++ src/Components/User.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default class User extends Component {
+  constructor() {
+    super();
+
+    this.state={
+      img: '',
+      name: '',
+      score: '',
+    }
+  }
+
+  render() {
+    const { img, name, score } = this.state;
+    return (
+      <div>
+        <header>
+          <p data-testid="header-profile-picture">{img}</p>
+          <p data-testid="header-player-name">{name}</p>
+          <p data-testid="header-score">{score}</p>
+        </header>
+      </div>
+    )
+  }
+}
