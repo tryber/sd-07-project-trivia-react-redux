48683372cbeedc336bff27dd41ccb181583ce427
alvaro <alvarovascon2@gmail.com>
2021-01-12 15:15:58 -0300

    finalizando requisito 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,21 @@
-export const getToken = () => {
-  fetch('https://opentdb.com/api_token.php?command=request')
+const tokenURL = 'https://opentdb.com/api_token.php?command=request';
+const getToken = () => fetch(tokenURL)
+  .then((response) => response.json())
+  .then((r) => {
+    localStorage.setItem('token', r.token);
+    return ({ token: r.token, date: new Date() });
+  })
+  .catch(() => {
+    localStorage.setItem('token', 'ERROR_TOKEN');
+    return ({ token: 'ERROR_TOKEN', date: 'ERROR_TOKEN' });
+  });
+const questionsBaseURL = 'https://opentdb.com/api.php?';
+const defaultAmount = 5;
+const questionsAPI = (amount = defaultAmount, token) => {
+  const questionsURL = `${questionsBaseURL}amount=${amount}&token=${token}`;
+  return fetch(questionsURL)
     .then((response) => response.json())
-    .then((data) => data);
+    .then((r) => r.results)
+    .catch(() => 'ERROR_QUESTIONS');
 };
-export const getQuestions = (token) => {
-  fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
-    .then((response) => response.json())
-    .then((data) => data);
-};
+export { getToken, questionsAPI };
