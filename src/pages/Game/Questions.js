3c975c54c9b1a97f8335bb8bbddd8ea664a0ddfa
Felipe Nascimento <flpnascto@gmail.com>
2021-01-11 23:53:49 -0300

    Requisição das perguntas na API Trivia

--- src/pages/Game/Questions.js
+++ src/pages/Game/Questions.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import * as triviaAPI from '../../services/triviaAPI';
 
 class Questions extends Component {
+  constructor() {
+    super();
+    this.fetchTriviaAPI = this.fetchTriviaAPI.bind(this);
+
+    this.state = {
+      questions: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchTriviaAPI();
+  }
+
+  async fetchTriviaAPI() {
+    const quantityOfQuestions = 5;
+    const token = localStorage.getItem('token');
+    const requestQuestions = await triviaAPI.getQuestions(quantityOfQuestions, token);
+    this.setState({
+      questions: requestQuestions.results,
+    });
+  }
+
   render() {
+    const { questions } = this.state;
+    console.log(questions);
     return (
       <div>
         <div data-testid="question-category">
