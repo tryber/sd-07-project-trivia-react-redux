a062aa0d15d11f537280e1243ee774c129161232
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-01-12 15:48:25 -0300

    fix: Ajusting components indes.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,10 @@
 export { default as LoginForm } from './LoginForm';
+export { default as Answer } from './Answer';
+export { default as ConfigForm } from './ConfigForm';
+export { default as Header } from './Header';
+export { default as Next } from './Next';
+export { default as Question } from './Question';
+export { default as RankingItem } from './RankingItem';
+export { default as Timer } from './Timer';
+export { default as AuthForm } from './AuthForm';
+export { default as FeedbackInfo } from './FeedbackInfo';
