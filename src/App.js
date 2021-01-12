72676dafc03ac45a6a0e6570c896b4648a4655f6
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-01-12 16:17:58 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch className="App">
       <Route exact path="/" component={ Login } />
+      <Route exact path="/game" component={ Game } />
     </Switch>
   );
 }
