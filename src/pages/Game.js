25538ea4861622a5f9b601f85d9b2090d1323f22
Fernando Antunes <fernando.antunes1@gmail.com>
2021-01-12 16:57:46 -0300

    Perguntas da página do jogo - apenas HTML

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/header';
+import Questions from '../components/questions';
 
 class Game extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Game extends React.Component {
       <div>
         Página do Jogo
         <Header />
+        <Questions />
       </div>
     );
   }
