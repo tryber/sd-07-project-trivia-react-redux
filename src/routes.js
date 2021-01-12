0c07981544eedfabddd935ec66f28097c4f130b5
Luiz Filipe <lfsmariz@gmail.com>
2021-01-12 09:24:00 -0300

    create scaffold route

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={ LoginPage } />
+    </Switch>
+  </BrowserRouter>;
+};
