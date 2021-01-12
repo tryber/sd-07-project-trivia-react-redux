8950680c92609aafe674cfd74aa64e36cab68e06
Carol Bezerra <carol.arbe@gmail.com>
2021-01-12 17:52:02 -0300

    Meio Requisito 5

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+import fetchThunk from '../redux/actions';
 
 class GamePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      atualAction: [],
+      index: 0,
+    };
+  }
+
+  componentDidMount() {
+    const { fetchHere } = this.props;
+    fetchHere();
+  }
+
   render() {
+    const { questions } = this.props;
     return (
       <div>
         <Header />
+        <p data-testid="question-category">{ category }</p>
+        <p data-testid="question-text">{ questions }</p>
+        <div>
+          map aqui
+        </div>
       </div>
     );
   }
 }
 
-export default GamePage;
+const mapStateToProps = (state) => ({
+  loading: state.loading,
+  questions: state.questions,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchHere: () => dispatch(fetchThunk()),
+});
+
+GamePage.propTypes = {
+  fetchHere: PropTypes.func.isRequired,
+  // loading: PropTypes.string.isRequired,
+  questions: PropTypes.shape({
+    responseCode: PropTypes.number.isRequired,
+    results: PropTypes.arrayOf(PropTyes.object).isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GamePage);
