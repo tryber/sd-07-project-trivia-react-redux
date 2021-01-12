10eb3911d26dd9bc9dd4f36d787d64dad878b78d
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-01-12 16:53:01 -0300

    criei a página de Ranking

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={ Login } exact />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
