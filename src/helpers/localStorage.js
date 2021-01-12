5fc9542ddab22ec1cf051e12540fd235b87d4ca1
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-01-12 10:19:31 -0300

    Função localStorage adicionada

---
+++ src/helpers/localStorage.js
@@ -0,0 +1,23 @@
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (error) {
+    // ignore write errors
+  }
+};
+
+// código desenvolvido por Jam Creencia
+// https://medium.com/@jrcreencia/persisting-redux-state-to-local-storage-f81eb0b90e7e
