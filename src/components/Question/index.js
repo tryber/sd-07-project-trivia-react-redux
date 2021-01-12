1cab8fba10e0e335d4a48bd76513c4f2c0363fbc
jvitorof <j.vitoroflores@gmail.com>
2021-01-12 18:58:37 -0300

    CSS: more style :sunglasses:

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './style.css'
 const Question = () => {
   return(
-    <div>
+    <section>
       <div className="message-box">
         <p className="message">Pergunta meus colegas alguma coisarada escriyta! pode ser maior ou menor</p>
       </div>
-    </div>
+    </section>
   )
 }
 
