2358fc29eb4e4cea8c1fac4b6d0fbe13e6591b5b
Galaraz <jvictor.si.2013@gmail.com>
2021-01-12 18:16:38 -0300

    req 3

---
+++ src/pages/SetPage/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SetPage extends Component {
+  render() {
+    return <h1 data-testid="settings-title">Settings</h1>;
+  }
+}
+
+export default SetPage;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import SetPage from './pages/SetPage';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ LoginPage } />
+      <Route path="/settings" component={ SetPage } />
     </Switch>
   </BrowserRouter>
 );
