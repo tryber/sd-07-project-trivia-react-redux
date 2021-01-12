f434a8dffa86b70a381222852df4b1703b0f1e9f
ANDREHORMAN <andrehormandev@gmail.com>
2021-01-11 22:08:03 -0300

    Ajustando EsLint - 11/01/2021

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,7 +6,7 @@ class Game extends Component {
       <div>
         <h1>TELA DE JOGO</h1>
       </div>
-    )
+    );
   }
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,7 @@ class Login extends React.Component {
       email: '',
       name: '',
       redirect: false,
-      token: ''
+      token: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -30,13 +30,13 @@ class Login extends React.Component {
 
     const info = {
       player: {
-        name: name,
+        name,
         assertions: 0,
         score: 0,
         gravatarEmail: email,
       },
-      ranking: [{ name: name, score: 10, picture: 'url-da-foto-no-gravatar' }],
-      token: token,
+      ranking: [{ name, score: 10, picture: 'url-da-foto-no-gravatar' }],
+      token,
     };
 
     localStorage.setItem('player', JSON.stringify(info));
@@ -46,10 +46,9 @@ class Login extends React.Component {
     const endpoint = 'https://opentdb.com/api_token.php?command=request';
     const response = await fetch(endpoint);
     const token = await response.json();
-    
     this.setState({
-      token: token
-    })
+      token,
+    });
   }
 
   handleSubmit() {
@@ -74,7 +73,7 @@ class Login extends React.Component {
   }
 
   render() {
-    const { email, name, redirect, } = this.state;
+    const { email, name, redirect } = this.state;
     if (redirect) return <Redirect to='/game' />;
     return (
       <div>
@@ -115,8 +114,8 @@ class Login extends React.Component {
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  sendEmail: (email) => dispatch(getEmail(email)),
+const mapDispatchToProps = dispatch => ({
+  sendEmail: email => dispatch(getEmail(email)),
 });
 
 Login.propTypes = {
