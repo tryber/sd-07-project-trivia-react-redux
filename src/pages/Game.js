2edfb9fc989f6464ee82c4eb04db21978e0fb96d
CleberFontinele <cleberfontinele2387@gmail.com>
2021-01-12 19:52:43 -0300

    arrumando erros do ESLint

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 class Game extends React.Component {
   render() {
-      return(
-          <div>
-              game
-          </div>
-      );
+    return (
+      <div>
+        game
+      </div>
+    );
   }
 }
 
