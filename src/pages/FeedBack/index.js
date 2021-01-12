25d124c60800bea330051af8364fcd9cbd5a26a1
Flavio Oliveira <flavsoliver@gmail.com>
2021-01-12 17:45:09 -0300

    requisito 13 - mensagem de feedback em 3+ acertos ou -3

--- src/pages/FeedBack/index.js
+++ src/pages/FeedBack/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../../components';
 
 class FeedBack extends Component {
   render() {
+    const { assertions } = this.props;
+    const threeAssertions = 3;
     return (
-      <Header />
+      <div>
+        <Header />
+        <p data-testid="feddback-text">
+          { assertions >= threeAssertions
+            ? 'Mandou bem!'
+            : 'Podia ser melhor...'}
+        </p>
+      </div>
     );
   }
 }
+
+FeedBack.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
 
 export default FeedBack;
