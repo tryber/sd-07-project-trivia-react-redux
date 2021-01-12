6b97d45f99c79029a13a56d041dc0536ad811eae
Lucas <lafdesouza2002@outlook.com>
2021-01-12 01:04:04 -0300

    Adicionando botao settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Settings extends React.Component {
   render() {
-    return <h1 data-testid="settings-title">Configurações</h1>;
+    return (
+      <div>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </div>
+    );
   }
 }
 
