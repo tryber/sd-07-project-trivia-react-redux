f277bf0cfabd3ecde6b3651af61af8fb18630f09
Lorena Góes <goesloren@gmail.com>
2021-01-12 20:10:57 -0300

    feat: config

---
+++ src/pages/Settings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <div>Configurações</div>
+    );
+  }
+}
+
+export default Settings;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Game from '../pages/Game';
+import Settings from '../pages/Settings';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={ Login } />
     <Route exact path="/jogo" component={ Game } />
+    <Route path="/config" component={ Settings } />
   </Switch>
 );
 
