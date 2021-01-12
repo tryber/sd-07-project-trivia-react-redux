11d0ec5d12e7c59d6be62ae45161c4d2ecf798df
Elisa <elisatcamillo@gmail.com>
2021-01-12 16:05:18 -0300

    estrutura da tela de feedback

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
 import './App.css';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/feedback" component={ Feedback } />
     </Switch>
   );
 }
---
+++ src/pages/Feedback.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Feedback extends Component {
+  render() {
+    const minAssertions = 3;
+    const { assertions, score } = this.props;
+    return (
+      <div>
+        <p data-testid="feedback-text">
+          {
+            assertions >= minAssertions
+              ? 'Mandou bem!'
+              : 'Podia ser melhor'
+          }
+        </p>
+        <h4>Placar Final</h4>
+        <p data-testid="feedback-total-score">{score}</p>
+        <h4>Perguntas Acertadas</h4>
+        <p data-testid="feedback-total-question">{assertions}</p>
+
+        <Link to="/">
+          <button data-testid="btn-play-again" type="button">
+            Jogar Novamente
+          </button>
+        </Link>
+
+        <Link to="/ranking">
+          <button data-testid="btn-ranking" type="button">
+            Ranking
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
