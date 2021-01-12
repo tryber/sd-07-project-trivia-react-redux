325b1b1719ff9b582948e0972780342618f829d1
gustasaideles <gustasaideles.gmail.com>
2021-01-12 19:07:16 -0300

    modificacoes do gravatarEmail

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
-import md5 from 'crypto-js/md5';
 
 class Header extends React.Component {
   render() {
     const { gravatarEmail, name, score } = this.props;
-    const md5Email = md5(gravatarEmail).toString();
-    const gravatar = `https://www.gravatar.com/avatar/${md5Email}`;
     return (
       <header>
         <img
           data-testid="header-profile-picture"
-          src={ gravatar }
+          src={ gravatarEmail }
           alt="imagem de avatar"
         />
         <p data-testid="header-player-name">
