6f255f7331add74aa5b1786a9ce515c318c9c7ee
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2021-01-13 15:01:14 -0300

    req7 tentativa

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchQuestions } from '../actions';
+//  import  './App.css'
 
 class Game extends Component {
   constructor() {
     super();
     this.renderAllDataQuestion = this.renderAllDataQuestion.bind(this);
+    this.handleUserAnswer = this.handleUserAnswer.bind(this);
     this.state = {
       questionIndex: 0,
+     correctAnswerValue: "",
+     wrongAnswerValue: "",
     };
   }
 
@@ -18,24 +22,54 @@ class Game extends Component {
   }
 
   renderAllDataQuestion() {
+    console.log()
     const { questionIndex } = this.state;
     const { questions } = this.props;
+
+   
     if (questions.results) {
       const correctAnswer = (
-        <button type="button" data-testid="correct-answer">
+        <button  type="button" 
+         data-testid="correct-answer" onClick={this.handleUserAnswer} id="ok">
           { questions.results[questionIndex].correct_answer }
         </button>
       );
-      const wrongAnswer = questions.results[
-        questionIndex
-      ].incorrect_answers.map((answer, index) => (
-        <button type="button" key={ answer } data-testid={ `wrong-answer-${index}` }>
+      const wrongAnswer = questions.results[questionIndex].incorrect_answers.map((answer, index) => (
+        <button   onClick={this.handleUserAnswer} type="button"  key={ answer } 
+        data-testid={ `wrong-answer-${index}`} id="notOk">
           {answer}
         </button>
       ));
       return [correctAnswer, ...wrongAnswer];
     }
   }
+
+  handleUserAnswer(e) {
+    const { id } = e.target
+    e.currentTarget.className = "btnColorRed"
+
+    if(id === "ok"){
+      e.currentTarget.className = "btnColorGreen"
+
+    } 
+  
+
+  //   const { questionIndex } = this.state;
+  //   const { questions } = this.props;
+
+  // this.setState({...this.state, correctAnswerValue: questions.results[questionIndex].correct_answer })
+  //  const {value } = e.target
+  //  console.log("oi",)
+  //  if(value=== this.state.correctAnswerValue){
+  //   e.currentTarget.className = "btnColorGreen"
+  //  } 
+  //   else(value !== this.state.correctAnswerValue)
+  //   { 
+  //     e.currentTarget.className = "btnColorRed"
+
+  //     }
+  }
+
 
   render() {
     const { questionIndex } = this.state;
