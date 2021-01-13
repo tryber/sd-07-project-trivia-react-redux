0b13e65c89f3466cca8470351697a4842a57980c
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-01-13 16:29:27 -0300

    item 3 requisito 11

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class Feedback extends React.Component {
+  
+  handleClick() {
+    const { history } = this.props;
+    history.push('/ranking');
+  }
+  
   render() {
     return (
       <div className="feedback">
         Tela de feedback
+        <button
+          onClick={ this.handleClick }
+          data-testid="btn-ranking"
+          type="button"
+        >
+          Ranking
+        </button>
       </div>
     );
   }
