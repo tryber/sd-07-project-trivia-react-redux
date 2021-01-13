c6344f53a21e9db1b9d387df4c928e59a5acca3b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-13 16:32:24 -0300

    formato inicial do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import JsGravatar from '@gravatar/js'
+import md5 from 'crypto-js/md5';
+
+// https://github.com/brix/crypto-js
+// md5 gera as hash para add no fim do link de acordo com o email do user.
 
 export default class Header extends Component {
   render() {
-    const userProfilePic = Gravatar({ email: 'example@example.com' });
-    console.log(userProfilePic);
     return (
       <header>
-
+        <img src={ `https://www.gravatar.com/avatar/${md5('email')}` } className="App-logo" alt="logo" />
+        <p data-testid="header-player">Nome</p>
+        <p data-testid="header-score">Score</p>
       </header>
     );
   }
