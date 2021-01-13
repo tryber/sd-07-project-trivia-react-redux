f21f07976ca6d5eb4a746ba8dd3453284786e16c
vanderson-henrique <vhbenedito@gmail.com>
2021-01-13 20:07:42 -0300

    Requisito 17 - agora foi

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import Header from '../Components/Header';
 import MessageFeedback from '../Components/MessageFeedback';
 import ScoreFeedback from '../Components/ScoreFeedback';
 
 class Feedback extends React.Component {
   componentDidMount() {
-    const { statePlayer: { nome, score, gravatarEmail } } = this.props;
+    const { player } = JSON.parse(localStorage.getItem('state'));
     const ranking = JSON.parse(localStorage.getItem('ranking'));
     const playerActual = {
-      name: nome,
-      score,
-      picture: gravatarEmail,
+      name: player.nome,
+      score: player.score,
+      picture: player.gravatarEmail,
     };
     ranking.push(playerActual);
     localStorage.setItem('ranking', JSON.stringify(ranking));
@@ -44,18 +42,4 @@ class Feedback extends React.Component {
   }
 }
 
-const mapStateToProps = ({ gameReducer }) => ({
-  statePlayer: gameReducer.statePlayer,
-});
-
-Feedback.propTypes = {
-  statePlayer: PropTypes.objectOf(
-    PropTypes.shape({
-      nome: PropTypes.string,
-      score: PropTypes.number,
-      gravatarEmail: PropTypes.string,
-    }),
-  ).isRequired,
-};
-
-export default connect(mapStateToProps)(Feedback);
+export default Feedback;
