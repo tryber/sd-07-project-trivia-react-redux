35cc3b9a781d6dc17fa857ea59a395d5c5b49680
ANDREHORMAN <andrehormandev@gmail.com>
2021-01-13 17:01:28 -0300

    Erro unexpect Json - 13/01/2021

--- src/actions/index.js
+++ src/actions/index.js
@@ -33,7 +33,6 @@ export function fetchQuestions() {
       const endpoint = `https://opentdb.com/api.php?amount=5&token=${token}`;
       const response = await fetch(endpoint);
       const data = await response.json();
-      data.responseType="text"
       console.log(data);
       dispatch(receivedQuestions(data));
     } catch (error) {
