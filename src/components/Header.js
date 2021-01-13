521dd0b55fe494f02da53538a04f7d63912973b8
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-13 15:44:29 -0300

    Instalaçao do Crypto-js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import JsGravatar from '@gravatar/js'
 
 export default class Header extends Component {
   render() {
+    const userProfilePic = Gravatar({ email: 'example@example.com' });
+    console.log(userProfilePic);
     return (
       <header>
-        xablauzin
+
       </header>
     );
   }
