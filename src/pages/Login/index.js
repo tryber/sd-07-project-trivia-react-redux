8768298cb0ef00f93a81e0bb1c7b453700c25e22
jvitorof <j.vitoroflores@gmail.com>
2021-01-13 15:30:35 -0300

    CSS: pages :metal:

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import { Header, Question, RankingItem } from '../../components';
+import { Header, Question, LoginForm, RankingItem } from '../../components';
 import './style.css';
 
 class Login extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Question />
-        <RankingItem />
+        <LoginForm />
       </div>
     );
   }
