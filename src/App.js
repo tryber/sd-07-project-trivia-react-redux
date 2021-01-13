0ed23c2fc337377e5ba0f1c2008553e489d850e6
marcos <marcos.1998226@gmail.com>
2021-01-13 15:53:16 -0300

    Criado pastas Action, Reducers, Store,  Helpers, Components e Servives

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-  
+        <p>
+          SUA VEZ
+        </p>
       </header>
     </div>
   );
