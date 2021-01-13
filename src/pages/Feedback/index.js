54c04513b7582917bc7a09c356a9307087965bd3
Carlos Souza <carlos.pessoal@hotmail.com>
2021-01-13 17:12:07 -0300

    Finalização do décimo terceiro requisito

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import Header from '../../components/Header';
+
 class Feedback extends Component {
   render() {
     const { assertions } = this.props;
     const sufficientAssertion = 3;
     return (
-      <div>
+      <main>
+        <Header />
         {
           assertions < sufficientAssertion
             ? (<p data-testid="feedback-text">Podia ser melhor...</p>)
             : (<p data-testid="feedback-text">Mandou bem!</p>)
         }
-      </div>
+      </main>
     );
   }
 }
