846cbbc943e2e22c84303c5205d837f6dc4266ba
Alana Monteiro <lanaah.15@gmail.com>
2021-01-13 17:14:02 -0300

    Requisito 7 - cor das bordas

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import Header from '../components/Header';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
 
 const Game = () => {
   const [counter, setCounter] = useState(0);
+  const [color, setColor] = useState({
+    style1: "",
+    style2: "",
+  });
+  // const [style2, setStyle2] = useState("");
   const handleQuestion = () => {
     const maxQuestions = 4;
     if (counter === maxQuestions) {
@@ -11,11 +16,25 @@ const Game = () => {
     } else {
       setCounter(counter + 1);
     }
+    setColor({
+      style1: "border-neutral",
+      style2: "border-neutral",
+    });
+  };
+
+  const handleClickAnswer = () => {
+    setColor({
+      style1: "border-correct",
+      style2: "border-incorrect",
+    });
+    // target.value==="correct" ? target.style.border="3px solid rgb(6, 240, 15)" : target.style.border="3px solid rgb(255, 0, 0)";
+    // setTimeout(() => {
+    //   target.style.border=style;
+    // }, 1000)
   };
 
   const dataGame = useSelector((state) => state.game);
   const { results } = dataGame.data;
-  console.log(results[counter]);
 
   return (
     <div>
@@ -27,19 +46,32 @@ const Game = () => {
         <p data-testid="question-text">{results[counter].question}</p>
       </div>
       <div>
-        <button type="button" data-testid="correct-answer">
+        <button
+          value="correct"
+          type="button"
+          data-testid="correct-answer"
+          className={color.style1}
+          onClick={handleClickAnswer}
+        >
           {results[counter].correct_answer}
         </button>
       </div>
       <div>
         {results[counter].incorrect_answers.map((answer, index) => (
-          <button key={ answer } type="button" data-testid={ `wrong-answer-${index}` }>
+          <button
+            value="incorrect"
+            key={answer}
+            type="button"
+            data-testid={`wrong-answer-${index}`}
+            className={color.style2}
+            onClick={handleClickAnswer}
+          >
             {answer}
           </button>
         ))}
       </div>
       <div>
-        <button type="button" data-testid="btn-next" onClick={ () => handleQuestion() }>
+        <button type="button" data-testid="btn-next" onClick={handleQuestion}>
           Próxima
         </button>
       </div>
