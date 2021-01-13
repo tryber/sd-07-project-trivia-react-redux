5b7708770e4b5d8a3fc24135832129f2fa257cf3
ANDREHORMAN <andrehormandev@gmail.com>
2021-01-13 19:18:48 -0300

    Contador feito e disable dos botões - 13/01/2021

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -10,14 +10,45 @@ class Game extends Component {
     super();
     this.renderAllDataQuestion = this.renderAllDataQuestion.bind(this);
     this.handleUserAnswer = this.handleUserAnswer.bind(this);
+    this.timer = this.timer.bind(this);
     this.state = {
       questionIndex: 0,
+      timer: 30,
+      disableButton: true,
     };
   }
 
   componentDidMount() {
     const { getQuestions } = this.props;
     getQuestions();
+  }
+
+  componentDidUpdate() {
+    this.timer();
+  }
+
+  timer() {
+    const { timer } = this.state;
+    if (timer > 1) {
+      setTimeout(() => {
+        this.setState(({ timer }) => ({
+          timer: timer -1,
+        }))
+      }, 1000);
+    }
+    if (timer === 0) {
+      this.handleUserAnswer();
+      this.setState({
+        timer: 30,
+        disableButton: true,
+      });
+    }
+    if (timer === 25) {
+      this.setState({
+        timer: 24,
+        disableButton: false,
+      });
+    }
   }
 
   handleUserAnswer() {
@@ -32,7 +63,7 @@ class Game extends Component {
 
   renderAllDataQuestion() {
     // console.log()
-    const { questionIndex } = this.state;
+    const { questionIndex, disableButton } = this.state;
     const { questions } = this.props;
 
     if (questions.results) {
@@ -43,6 +74,7 @@ class Game extends Component {
           onClick={ this.handleUserAnswer }
           key="correct"
           id="ok"
+          disabled={ disableButton }
         >
           { questions.results[questionIndex].correct_answer }
         </button>
@@ -55,6 +87,7 @@ class Game extends Component {
             key={ answer }
             data-testid={ `wrong-answer-${index}` }
             id="notOk"
+            disabled={ disableButton }
           >
             {answer}
           </button>
@@ -64,7 +97,7 @@ class Game extends Component {
   }
 
   render() {
-    const { questionIndex } = this.state;
+    const { questionIndex, timer } = this.state;
     const { questions } = this.props;
     return questions.results ? (
       <div>
@@ -77,6 +110,7 @@ class Game extends Component {
           {questions.results[questionIndex].question}
         </h2>
         <div>{this.renderAllDataQuestion()}</div>
+        <p>{ timer }</p>
       </div>
     ) : (
       <p>loading</p>
