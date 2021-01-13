54860f6a43075d72dc840c058ba834806051e4c0
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-01-12 23:57:45 -0300

    resolvi os requisitos do 12 ao 18

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   constructor(props) {
@@ -6,13 +7,20 @@ class Ranking extends React.Component {
     this.state = {
       rankings: JSON.parse(localStorage.getItem('ranking')) || [],
     };
+    this.redirectForHome = this.redirectForHome.bind(this);
+  }
+
+  redirectForHome(event) {
+    event.preventDefault();
+    const { history } = this.props;
+    if (history) history.push('/');
   }
 
   render() {
     const { rankings } = this.state;
     return (
       <div>
-        <h1>Ranking</h1>
+        <h1 data-testid="ranking-title">Ranking</h1>
         {rankings.map(({ name, score, picture }, index) => (
           <div key={ index }>
             <img src={ picture } alt="perfil" />
@@ -20,9 +28,19 @@ class Ranking extends React.Component {
             <span data-testid={ `player-score-${index}` }>{ score }</span>
           </div>
         ))}
+        <button
+          onClick={ this.redirectForHome }
+          data-testid="btn-go-home"
+        >
+          Voltar
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
 
 export default Ranking;
