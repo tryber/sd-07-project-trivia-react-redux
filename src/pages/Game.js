f643d5e6bb865b6a772dea3887a869aed6ee94c2
Kramer <kramerscs@gmail.com>
2021-01-13 05:43:02 -0300

    teste

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,6 +4,7 @@ class Game extends Component {
   render() {
     return (
       <div>
+        <h1>player info</h1>
         <h1>Token da requisição</h1>
         {localStorage.token}
       </div>
