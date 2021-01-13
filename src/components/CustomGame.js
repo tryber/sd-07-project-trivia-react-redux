cf54ae5cd42a0db881af3772ba579309bcb309b6
marcelo ivan <sagacello@hotmail.com>
2021-01-13 16:36:21 -0300

    tudo de bom

--- src/components/CustomGame.js
+++ src/components/CustomGame.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { scrambler } from '../services';
 
-const CustomGame = ({ challenge ,correct}) => (
-  <div>
-    {challenge.map(
-      ({ category, question, correct_answer, incorrect_answers }, index ) => {
+const CustomGame = ({ challenge, correct }) => {
+  
+  
+  return (
+    <div>
+      {challenge.map(({ category, question, correct_answer,incorrect_answers }, index) => {
+            const answersArray = scrambler([...incorrect_answers, correct_answer]);
+
         return (
           <div key={index}>
-            <p data-testid="question-category">{ category }</p>
-            <p data-testid="question-text">{ question }</p>
-            <button key={correct_answer} data-testid="correct-answer" onClick={ correct } >{ correct_answer }</button>
-            {incorrect_answers.map((item,index) => (
-              <button key={ index } data-testid={`wrong-answer-${index}`}>{item}</button>
-            ))}
+            <p data-testid="question-category">{category}</p>
+            <p data-testid="question-text">{question}</p>
+            {answersArray.map((item, index) => {
+            return <button
+                key={index}
+                onClick={correct}
+                data-testid={
+                  item === correct_answer
+                    ? 'correct-answer'
+                    : `wrong-answer-${index}`
+                }
+              >
+                {item}
+              </button>;
+            })}
           </div>
         );
-      }
-    )}
-  </div>
-);
+      })}
+    </div>
+  );
+};
 
 CustomGame.propTypes = {
   challenge: PropTypes.shape(PropTypes.array).isRequired,
---
+++ src/services/scrambler.js
@@ -0,0 +1,12 @@
+export default function scrambler(array) {
+    const newArray = [...array];
+    newArray.forEach(() => {
+      const randomIndex = Math.floor(Math.random() * newArray.length);
+      const randomElement = newArray[randomIndex];
+      const firstElement = newArray[0];
+      newArray[randomIndex] = firstElement;
+      newArray[0] = randomElement;
+    });
+    return newArray;
+  }
+  
