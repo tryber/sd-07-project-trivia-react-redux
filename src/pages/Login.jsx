eb4f0c7aac4ba08347fa068f2f3c9450e29f991a
vitorAmorims <vtamorims@gmail.com>
2021-01-13 10:24:02 -0300

    construçao gravatar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { fetchToken } from '../actions';
+import { fetchToken, fetchGravatar } from '../actions';
+import md5 from 'crypto-js/md5';
 
 class Login extends React.Component {
   constructor(props) {
@@ -40,21 +41,21 @@ class Login extends React.Component {
   }
 
   handleFetch() {
-    const { apiFetchToken } = this.props;
-    // const { token } = this.state;
-    // const newToken = await apiFetchToken();
+    const { apiFetchToken, apiFetchGravatar } = this.props;
+    const { email } = this.state;
+    const hash = md5(email).toString().toLocaleLowerCase().trim(); // https://www.gravatar.com/avatar/b463ad6c517f53d7b179ece3079c23ed
+    // console.log(hash);
+    apiFetchGravatar(hash);
     apiFetchToken();
     this.loadTokenToLocalStorage();
-    // this.setState({
-    //   token: newToken.token,
-    // });
   }
 
   handleChange(event) {
     event.preventDefault();
     // console.log(this.props);
     this.handleFetch();
-    console.log(this.state); // deve trazer o estado name, email e token. doneName e doneEmail false
+    
+    // console.log(this.state); // deve trazer o estado name, email e token. doneName e doneEmail false
     // this.loadTokenToLocalStorage(); //Logica para passar token do estado para localStorage
   }
 
@@ -71,7 +72,7 @@ class Login extends React.Component {
   }
 
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     // console.log(this.props);
     const { email, name, doneEmail, doneName } = this.state;
     return (
@@ -107,7 +108,7 @@ class Login extends React.Component {
             type="submit"
             data-testid="btn-play"
             disabled={ !doneName || !doneEmail }
-            onClick={ (event) => this.handleChange(event) }
+            onClick={ (event) => this.handleChange(event, email) }
           >
             Jogar
           </button>
@@ -124,15 +125,18 @@ class Login extends React.Component {
 
 const mapStateToProps = (state) => ({
   token: state.token,
+  gravatar: state.gravatar,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   apiFetchToken: () => dispatch(fetchToken()),
+  apiFetchGravatar: (value) => dispatch(fetchGravatar(value)),
 });
 
 Login.propTypes = {
   token: PropTypes.shape().isRequired,
   apiFetchToken: PropTypes.func.isRequired,
+  apiFetchGravatar: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
---
+++ src/reducers/asyncGravatar.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {
+    gravatar: '',
+  };
+  
+  const gravatar = (state = INITIAL_STATE, action) => {
+    const { type, hashData } = action;
+    console.log(action)
+    switch (type) {
+    case 'fetchSucessGravatar':
+      return { ...state, hashData };
+    default:
+      return state;
+    }
+  };
+  
+  export default gravatar;
+  
