4f4f447d7d331e7861778d0149a2755ecdeed4fa
Lorena Góes <goesloren@gmail.com>
2021-01-13 16:00:11 -0300

    Adding other elements to feedback page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      totalQuestions: 0,
+      score: 0,
+    };
+    this.handleClickPlayAgain = this.handleClickPlayAgain.bind(this);
+  }
+
+  handleClickPlayAgain() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
+    const { totalQuestions, score } = this.state;
+    const condition = 3;
     return (
-      <Header />
+      <div>
+        <Header />
+        <h2 data-testid="feedback-text">
+          {totalQuestions < condition ? 'Podia ser melhor...' : 'Mandou bem!' }
+        </h2>
+        <p data-testid="feedback-total-score">{score}</p>
+        <p data-testid="feedback-total-question">{totalQuestions}</p>
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ this.handleClickPlayAgain }
+        >
+          Jogar novamente
+        </button>
+      </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Feedback;
