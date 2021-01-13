789329c5963a5a7c14ddb193794d46bf60ccfd93
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-01-13 16:05:19 -0300

    Ida a tela de feedback após a 5 questão

---
+++ src/pages/Feedback.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    return (
+      <div className="feedback">
+        Tela de feedback
+      </div>
+    );
+  }
+}
+
+export default Feedback;
