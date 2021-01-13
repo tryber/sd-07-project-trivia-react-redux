447ceffccada76333b53466552e74c441a15a7fa
Lorena Góes <goesloren@gmail.com>
2021-01-13 14:59:53 -0300

    feat: feedback-ranking

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
+import Header from '.././components/Header'
 
 class Game extends Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Player Info</h1>
-          <img
-            data-testid="header-profile-picture"
-            src={ localStorage.email }
-            alt={ localStorage.username }
-          />
-          <h2 data-testid="header-player-name">{localStorage.username}</h2>
-          <h2>
-            Placar:
-            <span data-testid="header-score">0</span>
-          </h2>
-        </header>
+        <Header />
         <h1>Token da requisição</h1>
         {localStorage.token}
       </div>
