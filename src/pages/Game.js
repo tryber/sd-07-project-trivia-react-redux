b8cd6191fa41642393ec8fb4a197bdc3da46a1b2
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-13 20:32:26 -0300

    organizando css pagina games

---
+++ src/pages/Game.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Timer from 'react-compound-timer';
+// https://www.npmjs.com/package/react-compound-timer
+import GridQuestions from '../components/GridQuestions';
+import Header from '../components/Header';
+import './Game.css';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="header">
+          <Header />
+        </div>
+        <div className="gridQuestions">
+          <GridQuestions />
+        </div>
+        <div className="timer">
+          <Timer initialTime={ 30000 } direction="backward">
+            {() => (
+              <Timer.Seconds />)}
+          </Timer>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
