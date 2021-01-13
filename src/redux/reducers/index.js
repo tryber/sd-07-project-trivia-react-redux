723f9521ddeea3387a64d695720b001aeebc46b4
Felipe Nascimento <flpnascto@gmail.com>
2021-01-13 14:40:35 -0300

    Reducer para throwTimer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import token from './sessionReducer';
 import questionAnswererd from './questionAnswererd';
+import throwTimer from './throwTimer';
 
 export default combineReducers({
   token,
   questionAnswererd,
+  throwTimer,
 });
