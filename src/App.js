cc0884eace8ee0a26a9873cea3489ee247d74b5c
Julio Kauer <juliokauer@gmail.com>
2021-01-14 10:50:19 -0300

    Criação da page settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 import './App.css';
 
 export default function App() {
   return (
     <Switch>
+      <Route path="/settings" component={ Settings } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
