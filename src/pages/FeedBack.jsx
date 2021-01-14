6830e347e36ad47cee522cc0c2b68a56b541c10c
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-14 19:15:28 -0300

    requisito 13

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -3,10 +3,17 @@ import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   render() {
+    const playerStorage = JSON.parse(localStorage.getItem('state'));
+    console.log(playerStorage);
+    const three = 3;
     return (
       <div>
         <Header />
-        <div data-testid="feedback-text">Feedback Page</div>
+        {playerStorage.player.assertions >= three ? (
+          <div data-testid="feedback-text">Mandou bem!</div>
+        ) : (
+          <div data-testid="feedback-text">Podia ser melhor...</div>
+        )}
       </div>
     );
   }
