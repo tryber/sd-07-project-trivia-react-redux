efc35d4723709c672614d5ec1ac01e39962e5507
André Ferreira <andrefpires1@gmail.com>
2021-01-14 17:56:26 -0300

    Requisito 5

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
+export const getQuestions = (result) => ({
+  type: 'questions',
+  payload: result,
+});
+
 export const tokenAPI = (dataToken) => ({
   type: 'token',
   payload: dataToken,
@@ -6,6 +11,16 @@ export const tokenAPI = (dataToken) => ({
 export const getAPIToken = () => async (dispatch) => {
   const response = await fetch('https://opentdb.com/api_token.php?command=request');
   const data = await response.json();
+  const token = data.token;
 
-  dispatch(tokenAPI(data.token));
+  dispatch(tokenAPI(token));
 };
+
+export const getQuestions = () => async () => {
+  const token = localStorage.getItem('token');
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const data = await response.json();
+  const result = data.results;
+
+  dispatch(getQuestions(result));
+}
