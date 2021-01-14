7823f60ee9cb1a14d2d5bc401b0dd9b78d328b00
Felipe Nascimento <flpnascto@gmail.com>
2021-01-14 16:08:26 -0300

    Comitão Requisito 09

---
+++ src/pages/Game/Score.js
@@ -0,0 +1,17 @@
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+// class Score extends Component {
+
+//   render() {
+//     const { isAnswered } = this.props;
+//     console.log(`Score: ${isAnswered} `);
+//     return ('');
+//   }
+// }
+
+// const mapStateToProps = (state) => ({
+//   isAnswered: state.questionAnswererd.isAnswered,
+// });
+
+// export default connect(mapStateToProps)(Score);
