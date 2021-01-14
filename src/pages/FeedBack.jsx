e33121211bc5f440ae0ef9deeca72a9b5e2f57b6
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-14 18:54:27 -0300

    arrumando requisito 9

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class FeedBack extends React.Component {
   render() {
-    return <div data-testid="feedback-text">Feedback Page</div>;
+    return (
+      <div>
+        <Header />
+        <div data-testid="feedback-text">Feedback Page</div>
+      </div>
+    );
   }
 }
 
