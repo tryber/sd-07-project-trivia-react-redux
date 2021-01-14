d17e1322ec4d259ca72a022aed306431f121202b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-14 15:21:02 -0300

    Solving conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,10 @@ import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
 // https://github.com/brix/crypto-js
 // md5 gera as hash para add no fim do link de acordo com o email do user.
-import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
-<<<<<<< HEAD
     const { email, nome } = this.props;
-=======
-    const { nome, email } = this.props;
->>>>>>> e02782419880e1fafacc595305f1388c58dbdadc
     return (
       <header>
         <img src={ `https://www.gravatar.com/avatar/${md5(email)}` } className="App-logo" alt="logo" width="50" height="50" />
@@ -23,15 +18,6 @@ class Header extends Component {
   }
 }
 
-<<<<<<< HEAD
-const mapStateToProps = (state) => ({
-  email: state.player.email,
-  nome: state.player.name,
-});
-
-export default connect(mapStateToProps)(Header);
-// https://react-redux.js.org/using-react-redux/connect-mapstate
-=======
 Header.propTypes = {
   nome: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
@@ -43,4 +29,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Header);
->>>>>>> e02782419880e1fafacc595305f1388c58dbdadc
