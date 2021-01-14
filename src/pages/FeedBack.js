abcb2a5d93de14b48a7402372f081920510a149a
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-01-14 19:10:25 -0300

    feito req 15

--- src/pages/FeedBack.js
+++ src/pages/FeedBack.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export default class FeedBack extends Component {
+  constructor() {
+    super();
+    this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  handleRedirect() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <Header />
         feedback
         <p data-testid="feedback-text"> voce é top</p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.handleRedirect }
+        >
+          Jogar novamente
+        </button>
       </div>
     );
   }
 }
+
+FeedBack.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
