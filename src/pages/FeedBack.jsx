82fa7f2bec1caa4a676ca5610abdff414f973bed
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-01-14 17:01:26 -0300

    arrumando redirecionamento

--- src/pages/FeedBack.jsx
+++ src/pages/FeedBack.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class FeedBack extends React.Component {
   render() {
-    return <div>Feedback Page</div>;
+    return <div data-testid="feedback-text">Feedback Page</div>;
   }
 }
 
