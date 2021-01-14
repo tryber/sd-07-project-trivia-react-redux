c85904b7ec29cad01605bee89e1cb3e9bad8075d
CarolSi <a.carolg1@gmail.com>
2021-01-14 13:20:25 -0300

    main-group-27-css-header

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Header, Questions } from '../../components';
+import './Game.css';
 
 class Game extends Component {
   render() {
     return (
-      <>
+      <div className="game-page">
         <Header />
         <Questions />
-      </>
+      </div>
     );
   }
 }
