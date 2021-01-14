9f6816b86b98ee5b5a00dd34bed95b0254b69d2a
Fernando <nando.neves@gmail.com>
2021-01-14 14:57:13 -0300

    16

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
 class Ranking extends React.Component {
   render() {
