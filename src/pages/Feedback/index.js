f8c697b98c077a56c1568cb2714c91b0c1974c2e
Bruno Dantas <bruno.wesley77@gmail.com>
2021-01-15 15:15:18 -0300

    request 12

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const Feedback = () => (
   <div>
+    <header>
+      <Header />
+    </header>
     <h1 data-testid="feedback-text">Tela de Feedback</h1>
   </div>
 );
