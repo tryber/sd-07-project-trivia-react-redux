5a7db2459e914ac6b97fb34bc9c071e75452b770
Carlos Souza <carlos.pessoal@hotmail.com>
2021-01-15 19:18:05 -0300

    Ajustes finais

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { readRanking } from '../../services/storageService';
 
+import './index.css';
+
 class Ranking extends Component {
   constructor() {
     super();
@@ -11,11 +13,11 @@ class Ranking extends Component {
 
   renderRanking(ranking) {
     return ranking.map((player, index) => (
-      <div key={ index + 1 }>
-        <img alt={ player.name } src={ player.picture } />
-        <p data-testid={ `player-name-${index}` }>{player.name}</p>
-        <p data-testid={ `player-score-${index}` }>{player.score}</p>
-      </div>
+      <tr key={ index + 1 }>
+        <td><img alt={ player.name } src={ player.picture } /></td>
+        <td><p data-testid={ `player-name-${index}` }>{player.name}</p></td>
+        <td><p data-testid={ `player-score-${index}` }>{player.score}</p></td>
+      </tr>
     ));
   }
 
@@ -23,16 +25,30 @@ class Ranking extends Component {
     const { history } = this.props;
     const ranking = readRanking();
     return (
-      <div>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <div>{this.renderRanking(ranking)}</div>
-        <button
-          data-testid="btn-go-home"
-          type="button"
-          onClick={ () => history.push('/') }
-        >
-          Voltar
-        </button>
+      <div className="ranking__container">
+        <div className="ranking__content">
+          <h1 className="ranking__title" data-testid="ranking-title">Ranking</h1>
+          <table className="ranking__table">
+            <thead>
+              <tr>
+                <th>Foto</th>
+                <th>Nome</th>
+                <th>Pontos</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderRanking(ranking)}
+            </tbody>
+          </table>
+          <button
+            className="ranking__button"
+            data-testid="btn-go-home"
+            type="button"
+            onClick={ () => history.push('/') }
+          >
+            Voltar
+          </button>
+        </div>
       </div>
     );
   }
