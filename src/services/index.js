ca69a2663c668c3e19500369e6326db0d0f8a3cf
CarolSi <a.carolg1@gmail.com>
2021-01-15 18:42:18 -0300

    main-group-27-refatoracao-req-5

--- src/services/index.js
+++ src/services/index.js
@@ -6,10 +6,38 @@ async function fetchToken() {
   return result;
 }
 
+const five = 5;
+const getRandomNumber = () => Math.floor(Math.random() * five);
+
+function toRamQuestion(questions) {
+  const newAnswer = questions.map((question) => {
+    const answers = [question.correct_answer, ...question.incorrect_answers];
+    return answers.reduce((acc, answer, index) => {
+      acc[index] = { position: index, number: getRandomNumber(), answer };
+      return acc.sort((answer1, answer2) => answer2.number - answer1.number);
+    }, []);
+  });
+
+  const questionsOrdered = questions.map((question, index) => {
+    const newQuestion = {
+      question: question.question,
+      category: question.category,
+      answers: newAnswer[index],
+      difficulty: question.difficulty,
+    };
+    return newQuestion;
+  });
+  return questionsOrdered;
+}
+
 async function fetchQuestionNAnswer(token) {
   const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
   const result = await response.json();
-  return result.results;
+  const questions = result.results;
+  console.log(questions);
+  const readyQuestion = toRamQuestion(questions);
+
+  return readyQuestion;
 }
 
 export { fetchToken, fetchQuestionNAnswer };
