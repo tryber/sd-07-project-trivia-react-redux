4a9ab11604820b363bf6bc0225886aad3b0fe003
gustasaideles <gustasaideles.gmail.com>
2021-01-14 23:08:44 -0300

    corrigindo estrutura de arquivos

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import '../App.css';
-import { playerAction } from '../actions/playerAction';
+import '../../App.css';
+import { playerAction } from '../../actions/playerAction';
 
 class Quiz extends React.Component {
   constructor(props) {
