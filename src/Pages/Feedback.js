224a6dcdf88c8d069f6c8dbf3ee0d71610b5eb86
Isabella Paz <bellapaz.iorp@gmail.com>
2021-01-15 16:04:58 -0300

    Página de Feedback: Gerar mensagem de resultado e mostrá-la junto à pontuação total final e o número de acertos

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class Feedback extends Component {
-  /* constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-      mensagem: '',
+      resultMessage: {
+        badResult: 'Podia ser melhor...',
+        goodResult: 'Mandou bem!',
+      },
     };
-  } */
+  }
 
   render() {
+    const { score, assertions } = this.props;
+    const { resultMessage } = this.state;
+    const cutOff = 3;
     return (
       <div>
         <Header />
+        <div>
+          <h1>RESULTADO</h1>
+          <p data-testid="feedback-text">
+            { assertions >= cutOff ? resultMessage.goodResult : resultMessage.badResult }
+          </p>
+          <h2>
+            <h3>Pontuação Total: </h3>
+            <span data-testid="feedback-total-score">
+              {score === undefined ? 0 : score }
+            </span>
+          </h2>
+          <h2>
+            <h3>Total de Acertos:</h3>
+            <span data-testid="feedback-total-question">
+              {assertions === undefined ? 0 : score }
+            </span>
+          </h2>
+          <Link to="/">
+            <button
+              type="button"
+              data-testid="btn-play-again"
+            >
+              Jogar novamente
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
 }
-export default Feedback;
+
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertionsNumber,
+});
+
+Feedback.propTypes = {
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
