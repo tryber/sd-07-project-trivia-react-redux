2af6b6c56e0f5cf7bb7cbb546a326dc6a291f394
Fabio Camargos <fabiocmmcm@gmail.com>
2021-01-15 16:27:11 -0300

    requisito 13/14

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import Header from '../../components/Header';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Header } from '../../components';
 
-const Feedback = () => (
-  <div>
-    <header>
-      <Header />
-    </header>
-    <h1 data-testid="feedback-text">Tela de Feedback</h1>
-  </div>
-);
+class Feedback extends Component {
+  feedbackScore() {
+    const { assertions } = this.props;
+    const minScore = 3;
+    if (assertions < minScore) {
+      return (
+        <h2 data-testid="feedback-text">Podia ser melhor...</h2>
+      );
+    }
+    return (
+      <h2 data-testid="feedback-text">Mandou bem!</h2>
+    );
+  }
 
-export default Feedback;
+  render() {
+    return (
+      <div>
+        <header>
+          <Header />
+        </header>
+        <h1>Tela de Feedback</h1>
+        { this.feedbackScore() }
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => ({
+  assertions: state.game.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+export default connect(mapStateToProps)(Feedback);
