d649143a10fb8712844598d52776c42b9b3760e3
Luiz Filipe <lfsmariz@gmail.com>
2021-01-15 15:22:39 -0300

    remove Header for component feedback

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import Header from '../Header';
 
 class Feedback extends Component {
   constructor() {
