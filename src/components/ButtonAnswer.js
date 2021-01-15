37e4ddbf4455acffd971ca6d64c7880ec3db3808
Vitor Rodrigues <vitor_rc@outlook.com>
2021-01-15 18:43:59 -0300

    Agora vai

--- src/components/ButtonAnswer.js
+++ src/components/ButtonAnswer.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class ButtonAnswer extends React.Component {
   render() {
     const { onClick, disabled, dataTestid, className, item, showAnswers } = this.props;
-    console.log(dataTestid)
     return (
       <button
         onClick={ onClick }
@@ -17,3 +17,12 @@ export default class ButtonAnswer extends React.Component {
     );
   }
 }
+
+ButtonAnswer.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+  dataTestid: PropTypes.string.isRequired,
+  showAnswers: PropTypes.bool.isRequired,
+  className: PropTypes.string.isRequired,
+  item: PropTypes.string.isRequired,
+};
