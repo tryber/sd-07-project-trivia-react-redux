86f3d266df1d55c833e8e74925dfe5f27bb8e568
CarolSi <a.carolg1@gmail.com>
2021-01-15 12:54:03 -0300

    main-group-27-refatoracao do codigo, corrige lint

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Header from './Header';
 import Questions from './Questions';
-import Loading from './Loading';
 import FeedBackScore from './FeedbackScore';
 
-export { Header, Questions, Loading, FeedBackScore };
+export { Header, Questions, FeedBackScore };
