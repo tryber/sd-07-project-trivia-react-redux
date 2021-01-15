e5150f96c452634becad6388a4f149d3ada6ed96
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2021-01-15 15:52:48 -0300

    Requisito 18 resolvido

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ranking extends Component {
   render() {
     return (
       <div>
         <h2 data-testid="ranking-title">Ranking</h2>
+        <div>
+          <Link to="/">
+            <button
+              data-testid="btn-go-home"
+              type="button"
+            >
+              Voltar Inicio
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
