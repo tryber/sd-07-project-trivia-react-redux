4a7e3ae67a57647cf0fc83761d7675d47a4bdea1
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-01-15 21:08:40 -0300

    Requisito 5

--- src/pages/TelaDeJogo.js
+++ src/pages/TelaDeJogo.js
@@ -7,8 +7,9 @@ import { getQuestions } from '../actions';
 class TelaDeJogo extends Component {
   constructor() {
     super();
-    this.state = { index: 0, randomResponses: [] };
+    this.state = { next: 0 };
     this.handleQuestions = this.handleQuestions.bind(this);
+    this.nextQuestion = this.nextQuestion.bind(this);
   }
 
   componentDidMount() {
@@ -16,37 +17,84 @@ class TelaDeJogo extends Component {
     getAPIQuestions();
   }
 
-  handleQuestions() {
-    const { questions } = this.props;
-    const { index } = this.state;
-    let arr = [];
-    if (questions.length > 0) {
-      arr = [...questions[index].incorrect_answers, questions[index].correct_answer].sort();
+  handleQuestions(question) {
+    // const { questions } = this.props;
+    const { next } = this.state;
+    const randomResponses = [];
+    const number = 5;
+
+    if (question[next].correct_answer) {
+      randomResponses.push({
+        answer: question[next].correct_answer,
+        dataTestid: 'correct-answer',
+        randomIndex: Math.floor(Math.random() * number),
+      });
     }
+
+    if (question[next].incorrect_answers) {
+      question[next].incorrect_answers.forEach((element, index) => {
+        randomResponses.push({
+          answer: element,
+          dataTestid: `wrong-answer-${index}`,
+          randomIndex: Math.floor(Math.random() * index),
+        });
+      });
+    }
+
+    return randomResponses.sort((a, b) => a.randomIndex - b.randomIndex);
+  }
+
+  nextQuestion() {
+    this.setState((state) => ({
+      next: state.next + 1,
+    }));
   }
 
   render() {
-    const { email, name, score } = this.props;
+    const { email, name, score, questions } = this.props;
+    const { next } = this.state;
     const hash = md5(email);
-    this.handleQuestions();
+    // let random = [];
+    const avatarHash = `https://www.gravatar.com/avatar/${hash}`;
+
+    /*  if (questions.length) {
+      random = this.handleQuestions();
+    } */
+
     return (
       <div>
         <header>
           <h1>Tela de Jogo</h1>
           <img
             data-testid="header-profile-picture"
-            alt=""
-            src={ `https://www.gravatar.com/avatar/${hash}` }
+            alt="User Profile"
+            src={ avatarHash }
           />
           <div>
             <p data-testid="header-player-name">{name}</p>
           </div>
           <div>
-            <p data-testid="header-score">{score}</p>
+            <p data-testid="header-score">{ score || '0' }</p>
           </div>
         </header>
         <div>
-          <h3 data-testid="question-category">categoria</h3>
+          <p data-testid="question-category">
+            { questions.length && questions[next].category }
+          </p>
+          <p data-testid="question-text">
+            { questions.length && questions[next].question }
+          </p>
+          {
+            questions.length && this.handleQuestions(questions).map((option, i) => (
+              <button
+                type="button"
+                key={ i }
+                data-testid={ option.dataTestid }
+              >
+                { option.answer }
+              </button>))
+          }
+          <button type="button" onClick={ this.nextQuestion }> Next </button>
         </div>
       </div>
     );
