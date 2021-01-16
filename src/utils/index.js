d3b428263410f70b3b87a78816858e933a4f8ed1
Kramer <kramerscs@gmail.com>
2021-01-16 15:55:00 -0300

    feat: score board

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,6 +5,14 @@ const generateHash = (username, email) => {
   const sourceEmail = `https://www.gravatar.com/avatar/${emailHash}`;
   localStorage.setItem('email', sourceEmail);
   localStorage.setItem('username', username);
+  const player = { player: {
+    name: localStorage.username,
+    assertions: 0,
+    score: 0,
+    gravatar: localStorage.email,
+  },
+  };
+  localStorage.setItem('state', JSON.stringify(player));
 };
 
 export default generateHash;
