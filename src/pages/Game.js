7c1396b7393e65b1fddebec4c81a33be2c9d49dc
Lets <letsferreiratx@gmail.com>
2021-01-15 22:19:54 -0300

    rumo aos 80%

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { requestQuestionAndAnsewrs } from '../actions';
 import QuestionsList from './componentes/QuestionsList';
 import Header from './componentes/Header';
 import Loading from './componentes/Loading';
-import { Redirect } from 'react-router-dom';
 
 class Game extends React.Component {
   constructor(props) {
@@ -47,19 +47,19 @@ class Game extends React.Component {
 
     return (
       loading ? <Loading />
-      : (
-        <div>
-          <Header />
-          <h3>Category:</h3>
-          <p data-testid="question-category">{ question.results[index].category }</p>
-          <h3>Question:</h3>
-          <p data-testid="question-text">{ question.results[index].question }</p>
-          <QuestionsList
-            question={ question }
-            onClick={ this.nextQuestion }
-            index={ index }
-          />
-        </div>
+        : (
+          <div>
+            <Header />
+            <h3>Category:</h3>
+            <p data-testid="question-category">{ question.results[index].category }</p>
+            <h3>Question:</h3>
+            <p data-testid="question-text">{ question.results[index].question }</p>
+            <QuestionsList
+              question={ question }
+              onClick={ this.nextQuestion }
+              index={ index }
+            />
+          </div>
         )
     );
   }
