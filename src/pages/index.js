31cf73b2657a643f29a4727aae035c5a0f565129
Strongreen <strongreen@live.com>
2021-01-16 19:24:22 -0300

    correçoes de lint

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Game } from './Game';
+export { default as Login } from './Login';
+export { default as Ranking } from './Ranking';
+export { default as Settings } from './Settings';
