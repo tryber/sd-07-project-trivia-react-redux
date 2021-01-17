5f7f5440269ea38660d183c189ab6d9eaf1a71e0
Andre <dehhmenezes87@gmail.com>
2021-01-17 15:33:06 -0300

    Requisito 13 - Mensagem de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Header } from '../components';
+import EndGame from './EndGame';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.sendFeedback = this.sendFeedback.bind(this);
+  }
+
+  sendFeedback() {
+    const { history } = this.props;
+    history.push('/feedback');
+  }
+
   render() {
     const getLocalStorage = JSON.parse(localStorage.getItem('state'));
     const { player: { score } } = getLocalStorage;
@@ -14,13 +26,19 @@ class Feedback extends Component {
             className={ 'btn-visible' || 'btn-visible-hidden' }
             data-testid="btn-next"
             type="button"
+            onClick={ this.sendFeedback }
           >
             Próximo
           </button>
+          <EndGame />
         </div>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.objectOf.isRequired,
+};
 
 export default Feedback;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Login } from './Login';
 export { default as Ranking } from './Ranking';
 export { default as Settings } from './Settings';
 export { default as Feedback } from './Feedback';
+export { default as EndGame } from './EndGame';
