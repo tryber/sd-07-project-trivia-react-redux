0d64415cb149822967b4e23dc39a9ec10f855667
jvitorof <j.vitoroflores@gmail.com>
2021-01-18 20:16:36 -0300

    LOGIN: Images and some moves :metal:

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { LoginForm, ConfigButton } from '../../components';
 import './style.css';
 import angus from '../../images/angus.png';
+import longway from '../../images/longway.png';
 
 class Login extends Component {
   render() {
     return (
       <div className="loginPage-container">
         <div style={ { visibility: 'hidden' } }>a</div>
-        <img src={ angus } className="App-logo" alt="angus young" />
+        <div>
+          <div className="outer-image">
+            <img src={ longway } className="" alt="its a long way to the top" />
+            <div className="inner-image">
+              <img src={ angus } className="App-logo" alt="angus young" />
+            </div>
+          </div>
+        </div>
         <LoginForm />
         <ConfigButton />
       </div>
