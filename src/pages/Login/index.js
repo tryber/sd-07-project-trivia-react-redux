97bc040df1fcd6d7b11d389ad4f89a90773ea112
jvitorof <j.vitoroflores@gmail.com>
2021-01-18 14:14:21 -0300

    CONFIG: Page working :metal::rocket: logo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { LoginForm, ConfigButton } from '../../components';
 import './style.css';
-// import angus from '../../images/angus.pgn'
+import angus from './angus.pgn';
 
 class Login extends Component {
   render() {
     return (
       <div className="loginPage-container">
         <div style={ { visibility: 'hidden' } }>a</div>
-        <img src= alt="angus young" />
+        <img src={ angus } alt="angus young" />
         <LoginForm />
         <ConfigButton />
       </div>
