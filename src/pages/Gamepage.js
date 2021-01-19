3ff22bbe7bb7863a171d0596f82cdf44fc428ab3
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-01-19 10:39:33 -0300

    Alteração no nome do arquivo CSS

--- src/pages/Gamepage.js
+++ src/pages/Gamepage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import '../css/Button.css';
+import '../css/Buttons.css';
 import questionsAPI from '../services/questionAPI';
 import { addScoreboard } from '../actions';
 import Header from '../components/Header';
