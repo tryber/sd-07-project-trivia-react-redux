375d4c3074e88f30f00f482efdfa8c5ae148a829
wberilo <wberilo@gmail.com>
2021-01-20 00:44:11 -0300

    req 10, 11

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <p data-testid="feedback-text">Texto de feedback placeholder</p>
+      </div>
+    );
+  }
+}
+
+export default Game;
