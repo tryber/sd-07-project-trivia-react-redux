084fcf4feb694a4a724594e005991bd5ecac7ab8
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-01-20 21:30:18 -0300

    Finalizando Projeto Trivia - 20/01/2021

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import '../css/Ranking.css';
 import PropTypes from 'prop-types';
+import { getScore } from '../actions';
 
 class Ranking extends Component {
   constructor() {
@@ -9,14 +11,31 @@ class Ranking extends Component {
   }
 
   goToLogin() {
-    const { history } = this.props;
+    const { history, sendScore } = this.props;
+    sendScore(0);
     history.push('/');
   }
 
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
     return (
       <div className="ranking-container">
         <h1 data-testid="ranking-title">RANKING SCREEN</h1>
+        <ol>
+          {ranking.map((player, index) => (
+            <li key={ player }>
+              <div className="flex-box-ranking">
+                <img className="ranking-user-img" src={ player.picture } alt="img" />
+                <span data-testid={ `player-name-${index}` }>
+                  {player.name}
+                </span>
+                <span data-testid={ `player-score-${index}` }>
+                  {` - ${player.score} pontos`}
+                </span>
+              </div>
+            </li>))}
+        </ol>
+
         <button
           type="button"
           data-testid="btn-go-home"
@@ -29,10 +48,15 @@ class Ranking extends Component {
   }
 }
 
-export default Ranking;
+const mapDispatchToProps = (dispatch) => ({
+  sendScore: (score) => dispatch(getScore(score)),
+});
+
+export default connect(null, mapDispatchToProps)(Ranking);
 
 Ranking.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  sendScore: PropTypes.func.isRequired,
 };
